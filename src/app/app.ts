8802babfe15dd05f57580820765b83a0de1d9612
Saracier <105667230+Saracier@users.noreply.github.com>
2025-07-19 16:45:12 +0200

    feat: import tailind and daisyUI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
+import {Component, signal, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  private readonly http = inject(HttpClient);
   protected readonly title = signal('BuddyMakerFE');
+
+  protected members = signal<any>([])
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').
+      subscribe(members => {
+        console.log(members);
+        this.members.set(members)
+    })
+  }
+
 }
