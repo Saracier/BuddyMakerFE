9c4a6f8f97c22c308bbe6aa304aa0f2265e08d39
Saracier <105667230+Saracier@users.noreply.github.com>
2025-07-31 06:58:48 +0200

    feat: routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,13 @@
-import {Component, signal, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HttpClient} from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Nav } from "../layout/nav/nav";
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private readonly http = inject(HttpClient);
-  protected readonly title = signal('BuddyMakerFE');
-
-  protected members = signal<any>([])
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/members').
-      subscribe(members => {
-        console.log(members);
-        this.members.set(members)
-    })
-  }
-
+export class App {
+  protected router = inject(Router);
 }
